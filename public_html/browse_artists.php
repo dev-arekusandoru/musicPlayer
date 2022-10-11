6b5059c2d541bdc5b5e1ff01baf0ec4bb0aef600
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2022-10-11 11:05:50 -0400

    icon updatees

---
+++ public_html/browse_artists.php
@@ -0,0 +1,5 @@
+<?php
+
+include "../private_html/config.php";
+include_once PRIVATE_PATH . "dbConfig.php";
+
---
+++ public_html/profile.php
@@ -0,0 +1,6 @@
+<?php
+
+
+
+include "../private_html/config.php";
+include_once PRIVATE_PATH . "dbConfig.php";
