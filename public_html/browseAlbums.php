ebd5e58094c2ff8f2d66a87857f01d7c4d270d18
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2022-11-08 11:46:18 -0500

    more login changes

--- public_html/browseAlbums.php
+++ public_html/browseAlbums.php
@@ -6,27 +6,35 @@
 include "../private_html/config.php";
 include_once PRIVATE_PATH . "dbConfig.php";
 
-$albums = array();
+if (!isset($_SESSION['logged']) || $_SESSION['logged'] == 0) {
+    $_SESSION['page_to_load'] = "browseAlbums.php";
+    $smarty->display("userLogin.tpl");
+} else {
+    $albums = array();
 
-$stmt = $pdo->query("SELECT Album_ID, Album_Name, Album.Image_URL, Release_Year, Artist_Name FROM Album JOIN Artist ON Artist_FK=Artist.Artist_ID");
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $sql = "SELECT Album_ID, Album_Name, Album.Image_URL, Release_Year, Artist_Name FROM Album JOIN Artist ON Artist_FK=Artist.Artist_ID";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
 
-    $albums[] = $row;
-}
-
-//organize albums alphabetically
-for($i = 0; $i < count($albums); $i++) {
-    for($j = 0; $j < $i; $j++) {
-        if (strtolower($albums[$i]['Album_Name'][0]) < strtolower($albums[$j]['Album_Name'][0])) {
-            $temp = $albums[$i];
-            $albums[$i] = $albums[$j];
-            $albums[$j] = $temp;
-            break;
-        }
+    foreach ($stmt as $row) {
+        $albums[] = $row;
     }
-}
 
 //print("<pre>".print_r($albums,true)."</pre>");
-$smarty->assign('albums', $albums);
+//organize albums alphabetically
+    for ($i = 0; $i < count($albums); $i++) {
+        for ($j = 0; $j < $i; $j++) {
+            if (strtolower($albums[$i]['Album_Name'][0]) < strtolower($albums[$j]['Album_Name'][0])) {
+                $temp = $albums[$i];
+                $albums[$i] = $albums[$j];
+                $albums[$j] = $temp;
+                break;
+            }
+        }
+    }
 
-$smarty->display("browseAlbums.tpl");
+//print("<pre>".print_r($albums,true)."</pre>");
+    $smarty->assign('albums', $albums);
+
+    $smarty->display("browseAlbums.tpl");
+}
