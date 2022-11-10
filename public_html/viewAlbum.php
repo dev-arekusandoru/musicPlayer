70ef1ad3409c2165260750b54ab53623a60aa9aa
NathMali <nm1314@messiah.edu>
2022-11-10 11:03:53 -0500

    Update viewAlbum.php

--- public_html/viewAlbum.php
+++ public_html/viewAlbum.php
@@ -4,5 +4,73 @@
  *
  */
 include "../private_html/config.php";
+include_once PRIVATE_PATH . "dbConfig.php";
 
-$smarty->display("viewAlbum.tpl");
+
+$albumID = $_GET['id'];
+$songs = array();
+$album_info = [
+];
+
+$sql = "SELECT Album_ID, Album_Name, Album.Image_URL, Release_Year, Artist_ID, Artist_Name FROM Album JOIN Artist WHERE Album_ID=:id AND Artist_FK=Artist.Artist_ID;";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(":id", $albumID);
+$stmt->execute();
+$album_info[] = $stmt->fetch(PDO::FETCH_ASSOC);
+$album_info = $album_info[0];
+//print("<pre>" . print_r($album_info, true) . "</pre>");
+
+if (isset($album_info['Album_Name'])) {
+
+    $sql = "SELECT * FROM Song WHERE Artist_FK=:artist AND Album_FK=:album";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":artist", $album_info['Artist_ID']);
+    $stmt->bindParam(":album", $album_info['Album_ID']);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $songs[] = $row;
+    }
+
+//print("<pre>" . print_r($songs, true) . "</pre>");
+
+
+    if (isset($_POST['review'])) {
+        $comment = $_POST['review'];
+        $stars = $_POST['rating'];
+//    $comment = "Good";
+//    $stars = 5;
+        $user = 1;
+        $sql = "INSERT INTO Review (Comment, Stars, User_FK) VALUES (:comment, :stars, :user)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':comment', $comment);
+        $stmt->bindParam(':stars', $stars);
+        $stmt->bindParam(':user', $user);
+        $stmt->execute();
+    }
+//SELECT * FROM Album_Review WHERE Album_FK = :id;
+    $sql = "SELECT * FROM Review";//WHERE Review_FK = :id;
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $smarty->display("viewAlbum.tpl");
+    $reviews = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        foreach ($row as $key => $value) {
+            $reviews[$row['Review_ID']] = array(
+                'id' => $row['Review_ID'],
+                'rating' => $row['Stars'],
+                'comment' => $row['Comment'],
+                'user' => $row['User_FK'],
+            );
+        }
+    }
+
+    $smarty->assign('reviews',$reviews);
+    $smarty->assign("songs", $songs);
+    $smarty->assign("albumInfo", $album_info);
+    $smarty->display("viewAlbum.tpl");
+} else {
+    header("Location: explore.php");
+}
