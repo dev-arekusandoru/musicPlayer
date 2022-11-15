12f026d79f12dad7b8488a691d6526ca1fb85611
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2022-11-15 12:18:25 -0500

    Update git.php

--- public_html/git.php
+++ public_html/git.php
@@ -1,10 +1,11 @@
 <?php
 include "../private_html/config.php";
 
+echo exec('whoami');
 $old_path = getcwd();
 chdir('/var/www/private_html');
 exec('sh gitUpdate.sh');
 echo exec('sh gitUpdate.sh');
 chdir($old_path);
 
-header("Location: explore.php");
+//header("Location: explore.php");
