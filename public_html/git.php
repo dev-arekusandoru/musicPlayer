4774ed50e0e55327655aade6f2b9dd399fa9d052
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2022-11-15 12:48:25 -0500

    Update git.php

--- public_html/git.php
+++ public_html/git.php
@@ -6,7 +6,7 @@ chdir('/var/www/private_html');
 exec("ls -l", $items, $return_status);
 
 foreach ($items as $key => $item) {
-    printf("%s\n", $item);
+    printf("%s]<br>", $item);
 }
 /*
 exec('sh gitUpdate.sh');
