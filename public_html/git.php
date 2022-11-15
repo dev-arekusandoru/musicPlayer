56bc6ae26cba61f218c3af7ef6997298bfdf4845
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2022-11-15 11:39:55 -0500

    added images for the server

--- public_html/git.php
+++ public_html/git.php
@@ -1,7 +1,6 @@
 <?php
 $old_path = getcwd();
 chdir('/var/www/private_html');
-echo getcwd();
 exec('sh gitUpdate.sh');
 chdir($old_path);
 
--- public_html/libs/smarty-4.2.1/templates_c/ded9d20340a09a2090871b287a72d2cb458c39b2_0.file.addArtist.tpl.php
+++ public_html/libs/smarty-4.2.1/templates_c/ded9d20340a09a2090871b287a72d2cb458c39b2_0.file.addArtist.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-11-14 22:58:41
+/* Smarty version 4.2.1, created on 2022-11-15 11:36:11
   from '/Users/Arekusandoru/Programming/PhpStormProjects/musicPlayer/public_html/templates/addArtist.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_63730e712bc195_21942747',
+  'unifunc' => 'content_6373bffb9ff6c2_68916128',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ded9d20340a09a2090871b287a72d2cb458c39b2' => 
     array (
       0 => '/Users/Arekusandoru/Programming/PhpStormProjects/musicPlayer/public_html/templates/addArtist.tpl',
-      1 => 1668477161,
+      1 => 1668529627,
       2 => 'file',
     ),
   ),
@@ -20,30 +20,30 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_63730e712bc195_21942747 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6373bffb9ff6c2_68916128 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_66341797263730e712b1555_22648989', "title");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2810219396373bffb9f2814_69343848', "title");
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_109957905863730e712b65d4_04892589', "add");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_17428712606373bffb9f8b72_93903557', "add");
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_99779201863730e712b9714_00152881', "content");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_18112648646373bffb9fc329_34601520', "content");
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, "template.tpl");
 }
 /* {block "title"} */
-class Block_66341797263730e712b1555_22648989 extends Smarty_Internal_Block
+class Block_2810219396373bffb9f2814_69343848 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'title' => 
   array (
-    0 => 'Block_66341797263730e712b1555_22648989',
+    0 => 'Block_2810219396373bffb9f2814_69343848',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -53,12 +53,12 @@ Add Artist<?php
 }
 /* {/block "title"} */
 /* {block "add"} */
-class Block_109957905863730e712b65d4_04892589 extends Smarty_Internal_Block
+class Block_17428712606373bffb9f8b72_93903557 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'add' => 
   array (
-    0 => 'Block_109957905863730e712b65d4_04892589',
+    0 => 'Block_17428712606373bffb9f8b72_93903557',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -85,12 +85,12 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block "add"} */
 /* {block "content"} */
-class Block_99779201863730e712b9714_00152881 extends Smarty_Internal_Block
+class Block_18112648646373bffb9fc329_34601520 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_99779201863730e712b9714_00152881',
+    0 => 'Block_18112648646373bffb9fc329_34601520',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -106,7 +106,7 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
         <div class="row">
             <form class="row" action="addArtist.php" method="post" enctype="multipart/form-data">
                 <div class="add-img-div">
-                    <img class="add-img" id="add-img" src="img/pfp/blank.png" alt="">
+                    <img class="add-img" id="add-img" src="img/empty-playlist.jpg" alt="">
                     <input type="file" id="addArtistFile" name="img" accept=".jpg,.jpeg,.png" required onchange=file_changed()>
                     <?php echo '<script'; ?>
 >
